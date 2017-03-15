4840ace40502571b2f7f864b1e6fd07585d775b4
Mike Taylor <mike@whatsmycut.com>
2017-03-15 03:39:07 -0500

    Draft #2 ui touchups and tweaks to scripts

--- js/main.js
+++ js/main.js
@@ -1,31 +1,37 @@
 $(document).ready(function(){
-  var mktgdiv = $("#marketing")
-  var growdiv = $("#thegrow")
-  var ratsdiv = $("#dirtyrats")
-  var aboutdiv = $("#about")
-  var cdiv = "#marketing"
+  var mktgdiv = $("#marketing");
+  var growdiv = $("#thegrow");
+  var ratsdiv = $("#dirtyrats");
+  var aboutdiv = $("#about");
+  var indexdiv = $("index");
+  var cdiv = "#marketing";
+
   $(".growbutton").on('click', function(){
-    $(cdiv).hide(250);
-    $(growdiv).show(250);
+    $(cdiv).hide(265);
+    $(growdiv).show(265);
+    $(window).scrollTop(65)
     cdiv = "#thegrow";
   });
 
   $(".dirtyratsbutton").on('click', function(){
-    $(cdiv).hide(250);
-    $(ratsdiv).show(250);
+    $(cdiv).hide(265);
+    $(ratsdiv).show(265);
+    $(window).scrollTop(65)
     cdiv = "#dirtyrats";
   });
 
   $(".homebutton").on('click', function(){
-    $(cdiv).hide(250);
-    $(mktgdiv).show(250);
+    $(cdiv).hide(265);
+    $(mktgdiv).show(265);
+    $(window).scrollTop(65)
     cdiv = "#marketing";
   });
 
 
   $(".aboutbutton").on('click', function(){
-    $(cdiv).hide(250);
-    $(aboutdiv).show(250);
+    $(cdiv).hide(265);
+    $(aboutdiv).show(265);
+    $(window).scrollTop(65)
     cdiv = "#about";
   });
 
